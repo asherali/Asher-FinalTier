51b6dae704e0ad02d349b93bb78450e449fb0e89
asherali <asherali92@gmail.com>
2014-04-29 15:23:23 +0500

    Updated 29-4-14

--- Contours/src/simple_contours/Contours.java
+++ Contours/src/simple_contours/Contours.java
@@ -4,63 +4,78 @@ import org.opencv.core.Rect;
 public class Contours 
 {
 	double Height,Width,Area,diff,perc_variation,height_perc,width_perc;
-    double L1_area,L2_area,L3_area;
-	int L1_index,L2_index,L3_index;
-    
-   public Contours()
-   {
-	   L1_area=0;L2_area=0;L3_area=0;
-	   height_perc=0.25;
-	   width_perc=0.75;
-   }
-}
- class Biggest_contours extends Contours
-{
-	 Contours cont_L1=new Contours();
-	 	Contours cont_L2=new Contours();
-	 	Contours cont_L3=new Contours();
-    int counter;
-	 public void calc_big_3(Rect[] rect_arr,int conts_length)
-	    {
-	    	for(int i=0;i<conts_length;i++)
-	    	{
-	    		counter=i;
-	    		if(rect_arr[i].area()>L1_area)
-	    		{
-	    			if(cont_L1.Height<rect_arr[i].height||cont_L1.Width<rect_arr[i].width)
-	    			{
-	    				if((in_range(rect_arr[i].height, cont_L1.Height,height_perc))&&in_range(rect_arr[i].height, cont_L1.Height, width_perc))
-	    				{
-	    					L2_area=L1_area;
-	    					L2_index=L1_index;
-	    					cont_L2.Area=cont_L1.Area;
-	    					cont_L2.Height=cont_L1.Height;
-	    					cont_L2.Width=cont_L2.Width;
-	    					
-	    					L1_area=rect_arr[i].area();
-	    					L1_index=i;
-	    					cont_L1.Area=rect_arr[i].area();
-	    					cont_L1.Height=rect_arr[i].height;
-	    					cont_L1.Width=rect_arr[i].width;
-	    					
-	    					
-	    				}
-	    					    
-	    			}
-	    	    		
-	    		}
-	    		else if(rect_arr[i].area()<L1_area)
-	    		{
-	    			
-	    		}
-	    	}
-	    }
-	 
-	 
-	/* public void compare_two(Contours cont1,Contours cont2,Rect[] rect,double Largest_area)
-	 {
-	 }*/
-	 
+	
+   public double[] find_big4(Rect[] rect_arr,double[] L_rects,double[] big2,int cont_size, int[] cont_index, int[] big2_index)
+	{
+		L_rects[0]=rect_arr[0].area();
+		cont_index[0]=0;
+		
+	   for(int i=0;i<cont_size;i++)
+	   {
+		   if(rect_arr[i].area()>L_rects[0])
+		   {
+			   L_rects[1]=L_rects[0];
+			   L_rects[0]=rect_arr[i].area();
+			   cont_index[1]=cont_index[0];
+			   cont_index[0]=i;
+			   
+		   }
+		    if((rect_arr[i].area()>L_rects[1])&&(rect_arr[i].area()<L_rects[0]))
+		   {
+			   L_rects[2]=L_rects[1];
+			   L_rects[1]=rect_arr[i].area();
+			   cont_index[2]=cont_index[1];
+			   cont_index[1]=i;
+		   }
+		    if((rect_arr[i].area()>L_rects[2])&&(rect_arr[i].area()<L_rects[0])&&(rect_arr[i].area()<=L_rects[1]))
+		   {
+			   L_rects[3]=L_rects[2];
+			   L_rects[2]=rect_arr[i].area();
+			   cont_index[3]=cont_index[2];
+			   cont_index[2]=i;
+		   }
+		    if((rect_arr[i].area()>L_rects[3])&&(rect_arr[i].area()<L_rects[0])&&(rect_arr[i].area()<L_rects[1])&&(rect_arr[i].area()<L_rects[2]))
+		   {
+			   
+			   L_rects[3]=rect_arr[i].area();
+			   cont_index[3]=i;
+		   }
+	   }
+	   
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	   
+	   if(in_range(rect_arr[cont_index[0]].height,rect_arr[cont_index[1]].height, 25)&&in_range(rect_arr[cont_index[0]].width,rect_arr[cont_index[1]].width, 75))
+	   {
+		   big2[0]=L_rects[0];
+		   big2[1]=L_rects[1];
+		   big2_index[0]=cont_index[0];
+		   big2_index[1]=cont_index[1];
+		   
+	   }
+	   else
+	   {
+		   if(in_range(rect_arr[cont_index[1]].height,rect_arr[cont_index[2]].height, 25)&&in_range(rect_arr[cont_index[1]].width,rect_arr[cont_index[2]].width, 75))
+		   {
+			   big2[0]=L_rects[1];
+			   big2[1]=L_rects[2];
+			   big2_index[0]=cont_index[1];
+			   big2_index[1]=cont_index[2];
+			      
+		   }
+		   else
+		   {
+			   big2[0]=L_rects[2];
+			   big2[1]=L_rects[3];
+			   big2_index[0]=cont_index[2];
+			   big2_index[1]=cont_index[3];
+		   }
+		   
+		   return big2;
+	   }
+	   
+	return big2;
+	}
+   
 	 public boolean in_range(double big,double small,double perc)
 	 {
 		 diff=big-small;
--- Contours/src/simple_contours/Main.java
+++ Contours/src/simple_contours/Main.java
@@ -17,13 +17,14 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
+import com.recognition.software.jdeskew.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		Mat src_img,src_grey,src_blur,src_thresh,src_dilate,dest_img; 
-		src_img=Highgui.imread("n_num.jpg",Imgproc.COLOR_BGR2GRAY);
+		src_img=Highgui.imread("test9.jpg",Imgproc.COLOR_BGR2GRAY);
 		
 		
 		src_grey=new Mat(src_img.size(), Core.DEPTH_MASK_8U);
@@ -31,50 +32,91 @@ public class Main {
 		src_thresh=new Mat(src_img.size(), Core.DEPTH_MASK_8U);
 		src_dilate=new Mat(src_img.size(), Core.DEPTH_MASK_8U);
 		dest_img=Mat.zeros(src_img.size(), CvType.CV_8UC3);
-		//Core.bitwise_not(dest_img, dest_img);
-		Highgui.imwrite("dest.jpg", dest_img);
+		
+		
 		
 		Imgproc.cvtColor(src_img, src_grey, Imgproc.COLOR_BGR2GRAY);
 		Imgproc.GaussianBlur(src_grey, src_blur, new Size(3, 3), 0);
 		Imgproc.threshold(src_blur, src_thresh, 80, 255, Imgproc.THRESH_BINARY_INV);
 		Imgproc.dilate(src_thresh, src_dilate, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2)));
 		
+		Core.bitwise_not(src_dilate, src_dilate);
+		//currently finding contours for black bg and white fg
+		
+		Highgui.imwrite("dest.jpg", dest_img);
 		Highgui.imwrite("Threshold.jpg", src_thresh);
 		Highgui.imwrite("Dilate.jpg", src_dilate);
 
-		
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
 
 
 	      List<MatOfPoint> contours = new ArrayList<MatOfPoint>();  
 	      Mat heirarchy= new Mat();
 	      Point shift=new Point(0,0);
-	      Imgproc.findContours(src_dilate, contours,heirarchy, Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE,shift);
-	      double[] cont_area =new double[contours.size()]; 
-	      Rect[] rect_arr=new Rect[contours.size()];
+	      Imgproc.findContours(src_dilate, contours,heirarchy, Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_NONE,shift);
+	      //double[] cont_area =new double[contours.size()]; 
+	      int count=3;
+	      double height_perc= 0.50,width_perc=0.75;
+	       int cont_size=contours.size();
+	      Rect[] rect_arr=new Rect[cont_size];
+	      double[] L_rects= new double[4];
+	      double[] big2=new double[2];
+	      int[] cont_index=new int[4];
+	      int[] big2_index=new int[2];
+	      Rect rect;
 	      
-	         for(int i=0; i< contours.size();i++)
+	         for(int i=0; i<cont_size;i++)
 	         { 
-	            Rect rect = Imgproc.boundingRect(contours.get(i));
-	            cont_area[i]=Imgproc.contourArea(contours.get(i));
-	              rect_arr[i]=Imgproc.boundingRect(contours.get(i));
-	            
-	            System.out.println("Hight: "+rect.height);
-	            System.out.println("WIDTH: "+rect.width);
-	            //System.out.println("Cont AREA: "+cont_area[i]);
-	            System.out.println("rect AREA: "+rect_arr[i].area());
-	          //System.out.println(rect.x +","+rect.y+","+rect.height+","+rect.width);
-	              
-	              //System.out.println(rect_arr[i].x +","+rect_arr[i].y+","+rect_arr[i].height+","+rect_arr[i].width);
-	            if(rect.height>17)  
-	              Core.rectangle(src_img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,0,255));
-	             if(rect.height>17)
-	              Imgproc.drawContours(dest_img, contours, i, new Scalar(255,255,255),-1,8,heirarchy,2,shift);
-	              if(rect.height>17)
-	              Core.rectangle(dest_img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,255,0));
-	     }
+	        	 rect_arr[i]=Imgproc.boundingRect(contours.get(i));
+	        	 System.out.println("rect AREA: "+rect_arr[i].area());
+	         }
+	         
+	         Contours contObj= new Contours();
+	          big2=contObj.find_big4(rect_arr,L_rects,big2,cont_size,cont_index,big2_index);
+	         for(int i=0;i<4;i++)
+	  	   {
+	  		   System.out.println(L_rects[i]);
+	  		   System.out.println(cont_index[i]);
+	  	   }
+	         for(int i=0;i<2;i++)
+		  	   {
+		  		   System.out.println(big2[i]);
+		  		   System.out.println(big2_index[i]);
+		  	   }
+	         
+	         for(int i=0; i<cont_size;i++)
+	         { 
+	        	  rect = Imgproc.boundingRect(contours.get(i));
+		           // cont_area[i]=Imgproc.contourArea(contours.get(i));
+		            //System.out.println("Hight: "+rect.height);
+		            //System.out.println("WIDTH: "+rect.width);
+		            //System.out.println("Cont AREA: "+cont_area[i]);
+		            //System.out.println(rect.x +","+rect.y+","+rect.height+","+rect.width);
+		              //System.out.println(rect_arr[i].x +","+rect_arr[i].y+","+rect_arr[i].height+","+rect_arr[i].width);
+		            
+	        	 int[] iBuff=new int[(int)heirarchy.total()*heirarchy.channels()];
+	        	 heirarchy.get(0, 0,iBuff);
+	        	 //if(rect.height>17)
+	        	 if(iBuff[count]==-1)
+	        	 {
+	        		 if(contObj.in_range(rect_arr[big2_index[1]].height,rect_arr[i].height, height_perc))
+	        		 {
+	        			 
+		              Core.rectangle(src_img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,0,255));  	 
+		              Imgproc.drawContours(dest_img, contours, i, new Scalar(255,255,255),-1,8,heirarchy,2,shift);
+		             
+		              //Core.rectangle(dest_img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,255,0));
+	        	 }
+	        	 }
+		              count+=4; 
+	         }
+	         
+	         
               // Imgproc.erode(dest_img, dest_img, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2)));
  	         Highgui.imwrite("Final.jpg", dest_img);
 	         Highgui.imwrite("Original.jpg", src_img);
+	         
+	         
 	         
 	         File Filename=new File("final.jpg");
 	         Tesseract tesseract=Tesseract.getInstance();
@@ -91,5 +133,11 @@ public class Main {
 	   			System.err.println(e.getMessage());
 	   		}
 	}
+	
+	
+//////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	
 
 }
